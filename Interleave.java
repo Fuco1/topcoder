5219b24e32e63446bf948810451d42e7659ddc3d
Matus Goljer <dota.keys@gmail.com>
2014-09-19 16:24:44 +0200

    Add more tests and a lisp program to generate random interleaves

--- Interleave.java
+++ Interleave.java
@@ -20,6 +20,16 @@ public class Interleave {
         assert inter("foo", "baz", "fbazoo");
         assert inter("foo", "baz", "bfooaz");
         assert !inter("foo", "baz", "zaboof");
+        assert inter("chocolate", "chips", "cchocohilaptes");
+        assert !inter("chocolate", "chips", "chocochilatspe");
+        assert inter("loremipsumdolorsitametconsectetuer", "adipiscingelitpraesentliberoorciauctorsed"
+                     ,"loadreimippsumidsolocrsitamientgcelonitprsaecteetsueerntliberoorciauctorsed");
+        assert inter("loremipsumdolorsitametconsectetuer", "adipiscingelitpraesentliberoorciauctorsed"
+                     ,"ladoirpeimipssumcingelitpdorlaesoernstliitaberomeotrciaucctoronsecstetueedr");
+        assert inter("loremipsumdolorsitametconsectetuer", "adipiscingelitpraesentliberoorciauctorsed"
+                     ,"adlipiosrciengelmiitpspurmaesdeolontlirsibetramoeorctcoinsaeucctettuorersed");
+        assert inter("loremipsumdolorsitametconsectetuer", "adipiscingelitpraesentliberoorciauctorsed"
+                     ,"ladipoisrcingeemliitppsrumdolorsiaestaementlibteconrsoorecctietauuecrtorsed");
     }
 
     public static boolean inter(String a, String b, String c) {
@@ -30,3 +40,20 @@ public class Interleave {
             (b.charAt(0) == c.charAt(0) && inter(a, b.substring(1), c.substring(1)));
     }
 }
+
+// (interleave "loremipsumdolorsitametconsectetuer" "adipiscingelitpraesentliberoorciauctorsed")
+
+// (defun interleave (a b)
+//   (let ((first (string-to-list a))
+//         (second (string-to-list b))
+//         (out))
+//     (--dotimes (funcall (-on '+ 'length) a b)
+//       (let ((r (random 2)))
+//         (cond
+//          ((or (not first) (= r 1))
+//           (push (car second) out)
+//           (!cdr second))
+//          ((or (not second) (= r 0))
+//           (push (car first) out)
+//           (!cdr first)))))
+//     (apply 'string (nreverse out))))
